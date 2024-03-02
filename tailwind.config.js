d29a0e35e0d367071b2d3df9403eaa6760e33b72
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-03 01:36:41 +0300

    Configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx'
+    './index.html',   // Add the path to your main HTML file
+    './*.html',
   ],
   theme: {
     extend: {}
